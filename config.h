0328efd68879610347e149d2e3849d7449f70402
user7210unix <user7210@proton.me>
2025-02-03 12:21:56 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,4 +1,19 @@
-#define TERMINAL "st"
-#define DESKTOPS 5
-#define MOD_KEY Mod1Mask // ALT key
+#pragma once
 
+// Appearance
+#define BORDER_SIZE 2
+#define GAP_SIZE 10
+#define FOCUSED_COLOR "#268bd2"
+#define NORMAL_COLOR "#002b36"
+#define BAR_HEIGHT 24
+#define FONT "monospace-12"
+
+// Keybindings
+#define MOD_KEY Mod1Mask  // Alt key
+#define TERMINAL "st"
+#define DMENU_CMD "dmenu_run -p 'Run:'"
+#define BACKGROUND_CMD "feh --bg-fill ~/wallpaper.jpg"
+
+// Workspaces
+#define DESKTOPS 5
+#define MAX_WINDOWS 20
